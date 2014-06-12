0f4fb58660f5b8b987e1dd9260766c7e8f15b171
DimaPhil <dima239.philippov@gmail.com>
2014-06-12 17:02:22 +0400

    Template for TopCoder was added

---
+++ Templates/main_tc.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+#define EPS (1e-9)
+#define INF (1e9)
+#define INFLONG (long long)(1e18)
+#define sz(a) (int)a.size()
+#define all(a) (a).begin(), (a).end()
+#define fst first
+#define snd second
+#define pb push_back
+#define mp make_pair
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector <int> vi;
+typedef vector <ll> vll;
+typedef vector <bool> vb;
+typedef pair <int, int> pii;
+typedef vector <pii> vpii;
