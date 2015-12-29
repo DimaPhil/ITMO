4dd8611fbb83302509d4745d4bf2613c6b9044c0
DimaPhil <dima239.philippov@gmail.com>
2015-12-29 21:30:13 +0300

    problem F - not finished yet

--- Automaton programming/FContinuous/main.cpp
+++ Automaton programming/FContinuous/main.cpp
@@ -12,14 +12,14 @@ char in[maxlen + 1];
 double out[maxlen + 1];
 double f[2][maxn][alphabet];
 
-int gauss(std::vector<std::vector<double>> matrix, std::vector<double> &ans) {
+int gauss(std::vector<std::vector<double>> matrix, std::vector<double>& answer) {
     size_t n = matrix.size();
     size_t m = matrix[0].size() - 1;
-    vector<int> where(m, -1);
+    std::vector<int> where(m, -1);
     for (size_t column = 0, row = 0; column < m && row < n; column++) {
         size_t minimal_row = row;
-        for (int i = row; i < n; i++) {
-            if (std::abs(matrix[i][column]) > std::abs(matrix[sel][column])) {
+        for (size_t i = row; i < n; i++) {
+            if (std::abs(matrix[i][column]) > std::abs(matrix[minimal_row][column])) {
                 minimal_row = i;
             }
         }
@@ -27,7 +27,7 @@ int gauss(std::vector<std::vector<double>> matrix, std::vector<double> &ans) {
             continue;
         }
         for (size_t i = column; i <= m; i++) {
-            std::swap(matrix[sel][i], matrix[row][i]);
+            std::swap(matrix[minimal_row][i], matrix[row][i]);
         }
         where[column] = row;
         for (size_t i = 0; i < n; i++) {
@@ -41,23 +41,23 @@ int gauss(std::vector<std::vector<double>> matrix, std::vector<double> &ans) {
         row++;
     }
  
-    ans.resize(m, 0);
+    answer.resize(m, 0);
     for (size_t i = 0; i < m; i++) {
         if (where[i] != -1) {
-            ans[i] = matrix[where[i]][m] / matrix[where[i]][i];
+            answer[i] = matrix[where[i]][m] / matrix[where[i]][i];
         }
     }
     for (size_t i = 0; i < n; i++) {
         double sum = 0;
         for (size_t j = 0; j < m; j++) {
-            sum += ans[j] * matrix[i][j];
+            sum += answer[j] * matrix[i][j];
         }
         if (std::abs(sum - matrix[i][m]) > EPS) {
             return 0;
         }
     }
  
-    for (int i = 0; i < m; i++) {
+    for (size_t i = 0; i < m; i++) {
         if (where[i] == -1) {
             return INF;
         }
@@ -66,8 +66,8 @@ int gauss(std::vector<std::vector<double>> matrix, std::vector<double> &ans) {
 }
 
 int main() {
-  freopen("discrete.in", "r", stdin);
-  freopen("discrete.out", "w", stdout);
+  freopen("continuous.in", "r", stdin);
+  freopen("continuous.out", "w", stdout);
   int n, m;
   scanf("%d%d", &n, &m);
   for (int i = 0; i < n; i++) {
@@ -81,11 +81,34 @@ int main() {
   std::vector<std::vector<double>> matrix(2 * n, std::vector<double>(2 * n + 1));
   for (int it = 0; it < m; it++) {
     scanf("%d%s", &len[it], in);
-    for (int i = 0; i < len; i++) {
-        scanf("%f", out[i]);
+    for (int i = 0; i < len[it]; i++) {
+        scanf("%lf", &out[i]);
     }
     int state = 0;
-    
+    std::vector<int> variables;
+    for (int i = 0; i < len[it]; i++) {
+        int new_varible = state * 2 + 1;
+        if (in[i] == '0') {
+            new_varible = state * 2;
+        }
+        variables.push_back(new_varible);
+        for (size_t j = 0; j < variables.size(); j++) {
+            for (size_t k = 0; k < variables.size(); k++) {
+                matrix[variables[j]][variables[k]] += 2. / len[it];
+            }
+            matrix[variables[j]][2 * n] += 2. * out[i] / len[it];
+        }
+        if (in[i] == '0') {
+            state = go[state][0];
+        } else {
+            state = go[state][1];
+        }
+     }
+  }
+  std::vector<double> answer;
+  assert(gauss(matrix, answer) == 1);
+  for (int i = 0; i < n; i++) {
+    printf("%.10f %.10f\n", answer[2 * i], answer[2 * i + 1]);
   }
   return 0;
 }
